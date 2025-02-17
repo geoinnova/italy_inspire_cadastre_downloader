6a5551c7a88bfd9fb685b313ddc92adc22943d57
miquelfm <miquel.febrer@geoinnova.org>
2025-02-17 14:20:51 +0100

    v0.1

---
+++ i18n/af_it.pro
@@ -0,0 +1,4 @@
+FORMS = ../Italy_Inspire_Cadastre_Downloader_dialog_base.ui
+SOURCES = ../Italy_Inspire_Cadastre_Downloader_dialog.py \ 
+../Italy_Inspire_Cadastre_Downloader.py
+TRANSLATIONS = af_it.ts
---
+++ i18n/af_it.ts
@@ -0,0 +1,128 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT" sourcelanguage="en">
+<context>
+    <name>ItalyInspireCadastreDownloader</name>
+    <message>
+        <location filename="../Italy_Inspire_Cadastre_Downloader.py" line="181"/>
+        <source>&amp;Italy Inspire Cadastre Downloader</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../Italy_Inspire_Cadastre_Downloader.py" line="168"/>
+        <source>Italy Inspire Cadastre Downloader</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>ItalyInspireCadastreDownloaderDialog</name>
+    <message>
+        <location filename="../Italy_Inspire_Cadastre_Downloader_dialog.py" line="132"/>
+        <source>An error occurred while request data. Code error: </source>
+        <translation>Si è verificato un errore durante la richiesta dei dati. Errore nel codice:</translation>
+    </message>
+    <message>
+        <location filename="../Italy_Inspire_Cadastre_Downloader_dialog.py" line="199"/>
+        <source>An error occurred while decompressing the file</source>
+        <translation>Si è verificato un errore durante la decompressione del file</translation>
+    </message>
+    <message>
+        <location filename="../Italy_Inspire_Cadastre_Downloader_dialog.py" line="233"/>
+        <source>The folder already exists</source>
+        <translation>La cartella esiste già</translation>
+    </message>
+    <message>
+        <location filename="../Italy_Inspire_Cadastre_Downloader_dialog.py" line="240"/>
+        <source>You must add a download folder</source>
+        <translation>È necessario aggiungere una cartella di download</translation>
+    </message>
+    <message>
+        <location filename="../Italy_Inspire_Cadastre_Downloader_dialog.py" line="252"/>
+        <source>Download successfuly completed</source>
+        <translation>Download completato con successo</translation>
+    </message>
+    <message>
+        <location filename="../Italy_Inspire_Cadastre_Downloader_dialog.py" line="260"/>
+        <source>You must select a municipality</source>
+        <translation>È necessario selezionare un comune</translation>
+    </message>
+    <message>
+        <location filename="../Italy_Inspire_Cadastre_Downloader_dialog.py" line="269"/>
+        <source>You must complete the data of the province and municipality and indicate the download route.</source>
+        <translation>È necessario completare i dati della provincia e del comune e indicare il percorso di download.</translation>
+    </message>
+    <message>
+        <location filename="../Italy_Inspire_Cadastre_Downloader_dialog.py" line="270"/>
+        <source>You must select at least one cadastral entity to download.</source>
+        <translation>È necessario selezionare almeno un&apos;entità catastale da scaricare.</translation>
+    </message>
+    <message>
+        <location filename="../Italy_Inspire_Cadastre_Downloader_dialog.py" line="103"/>
+        <source>Select folder</source>
+        <translation>Seleziona la cartella</translation>
+    </message>
+    <message>
+        <location filename="../Italy_Inspire_Cadastre_Downloader_dialog.py" line="152"/>
+        <source>Select a region</source>
+        <translation>Seleziona una regione</translation>
+    </message>
+    <message>
+        <location filename="../Italy_Inspire_Cadastre_Downloader_dialog.py" line="166"/>
+        <source>Select a province</source>
+        <translation>Seleziona una provincia</translation>
+    </message>
+    <message>
+        <location filename="../Italy_Inspire_Cadastre_Downloader_dialog.py" line="246"/>
+        <source>Select a municipality</source>
+        <translation>Seleziona un comune</translation>
+    </message>
+</context>
+<context>
+    <name>ItalyInspireCadastreDownloaderDialogBase</name>
+    <message>
+        <location filename="../Italy_Inspire_Cadastre_Downloader_dialog_base.ui" line="14"/>
+        <source>Italy Inspire Cadastre Downloader</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../Italy_Inspire_Cadastre_Downloader_dialog_base.ui" line="50"/>
+        <source>Municipality</source>
+        <translation>Comune</translation>
+    </message>
+    <message>
+        <location filename="../Italy_Inspire_Cadastre_Downloader_dialog_base.ui" line="82"/>
+        <source>Region</source>
+        <translation>Regione</translation>
+    </message>
+    <message>
+        <location filename="../Italy_Inspire_Cadastre_Downloader_dialog_base.ui" line="114"/>
+        <source>Province</source>
+        <translation>Provincia</translation>
+    </message>
+    <message>
+        <location filename="../Italy_Inspire_Cadastre_Downloader_dialog_base.ui" line="137"/>
+        <source>Add to map</source>
+        <translation>Aggiungi alla mappa</translation>
+    </message>
+    <message>
+        <location filename="../Italy_Inspire_Cadastre_Downloader_dialog_base.ui" line="152"/>
+        <source>Download folder</source>
+        <translation>Scarica la cartella</translation>
+    </message>
+    <message>
+        <location filename="../Italy_Inspire_Cadastre_Downloader_dialog_base.ui" line="180"/>
+        <source>...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../Italy_Inspire_Cadastre_Downloader_dialog_base.ui" line="192"/>
+        <source>Download cadastral data</source>
+        <translation>Scarica i dati catastali</translation>
+    </message>
+    <message>
+        <location filename="../Italy_Inspire_Cadastre_Downloader_dialog_base.ui" line="256"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;If you want to get more information visit: &lt;a href=&quot;https://geoinnova.org/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Geoinnova&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Se vuoi avere maggiori informazioni visita: &lt;a href=&quot;https://geoinnova.org/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Geoinnova&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+</context>
+</TS>
